b33c52dfd2a3691e09c5ee6f83105ff17e728d51
Tiago Vazzoller <tiago_rv0@hotmail.com>
2021-10-02 17:28:11 -0300

    Aula 1 API Rest Alura

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,87 @@
+﻿using FilmesAPI.Controllers.Models;
+using FilmesAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//instalar para o banco de dados: Microsoft.EntityFrameworkCore / Microsoft.EntityFrameworkCore.Tools / MySql.Data.EntityFrameworkCore
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        //private static List<Filme> filmes = new List<Filme>(); // nao precisa mais devido ao banco de dados
+        //private static int id = 1; // nao precisa mais devido ao banco de dados
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost] //pega os dados 
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            //filme.Id = id++;
+            //filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaFilmesPorID), new { Id = filme.Id }, filme); //retorna o local
+        }
+
+        [HttpGet] //envia os dados
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return _context.Filmes;
+        }
+
+        [HttpGet("{id}")] //envia um parametro para requisição
+        public IActionResult RecuperaFilmesPorID(int id)
+        {
+            
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")] //atualiza 
+        public IActionResult AtualizaFilme(int id, [FromBody] Filme filmeNovo)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+
+            }
+            filme.Titulo = filmeNovo.Titulo;
+            filme.Genero = filmeNovo.Genero;
+            filme.Diretor = filmeNovo.Diretor;
+            filme.Duracao = filmeNovo.Duracao;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")] //deleta
+        public IActionResult DeletaFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+
+            }
+
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ FilmesAPI/Controllers/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O campo Titulo é obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+
+        [Range(1, 240, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 240 minutos")]
+        public int Duracao { get; set; }
+    }
+}
